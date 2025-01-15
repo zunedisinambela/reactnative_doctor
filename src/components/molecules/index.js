3d787af7dcb44ff38ec87b3ed1e9dcab22fbef90
zunedisinambela <zunedisinambela@gmail.com>
2025-01-15 23:08:24 +0700

    [style] message screen

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -4,5 +4,14 @@ import HomeProfile from "./HomeProfile";
 import DoctorCategory from "./DoctorCategory";
 import RatedDoctor from "./RatedDoctor";
 import NewsItem from "./NewsItem";
+import ListDoctor from "./ListDoctor";
 
-export { Header, BottomNavigator, HomeProfile, DoctorCategory, RatedDoctor, NewsItem }
+export {
+  Header,
+  BottomNavigator,
+  HomeProfile,
+  DoctorCategory,
+  RatedDoctor,
+  NewsItem,
+  ListDoctor
+}
--- src/pages/MessagesScreen.js
+++ src/pages/MessagesScreen.js
@@ -1,14 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ListDoctor } from '../components'
+import { colors, fonts } from '../utils'
 
 const MessagesScreen = () => {
   return (
-    <View>
-      <Text>MessagesScreen</Text>
+    <View style={styles.page}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Messages</Text>
+        <ListDoctor />
+        <ListDoctor />
+        <ListDoctor />
+      </View>
     </View>
   )
 }
 
 export default MessagesScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginLeft: 16
+  }
+})
