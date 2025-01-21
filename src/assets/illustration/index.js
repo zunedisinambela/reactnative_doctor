5030560d08ff8f695a938734c42ad54f6a8f01da
zunedisinambela <zunedisinambela@gmail.com>
2025-01-21 22:48:50 +0700

    [style] hospital screen

--- src/assets/illustration/index.js
+++ src/assets/illustration/index.js
@@ -4,6 +4,7 @@ import ILNullPhoto from './null-photo.png'
 import ILCatUmum from './cat-dok-umum.svg'
 import ILCatPsikiater from './cat-dok-psikiater.svg'
 import ILCatObat from './cat-dok-obat.svg'
+import ILHospitalBG from './hospitals-background.png'
 
 export { 
   ILLogo, 
@@ -11,5 +12,6 @@ export {
   ILNullPhoto,
   ILCatUmum,
   ILCatPsikiater,
-  ILCatObat
+  ILCatObat,
+  ILHospitalBG,
 }
--- src/pages/HospitalsScreen.js
+++ src/pages/HospitalsScreen.js
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground } from 'react-native'
 import React from 'react'
+import { ILHospitalBG } from '../assets'
+import { colors, fonts } from '../utils'
+import { ListHospital } from '../components'
 
 const HospitalsScreen = () => {
   return (
-    <View>
-      <Text>HospitalsScreen</Text>
+    <View style={styles.page}>
+      <ImageBackground source={ILHospitalBG} style={styles.background} >
+        <Text style={styles.title}>Nearby Hospitals</Text>
+        <Text style={styles.desc}>3 tersedia</Text>
+      </ImageBackground>
+      <View style={styles.content}>
+        <ListHospital />
+        <ListHospital />
+        <ListHospital />
+      </View>
     </View>
   )
 }
 
 export default HospitalsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  background: {
+    height: 240,
+    paddingTop: 30,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.white,
+    textAlign: 'center',
+  },
+  desc: {
+    fontSize: 14,
+    fontFamily: fonts.primary[300],
+    color: colors.white,
+    marginTop: 6,
+    textAlign: 'center',
+  },
+  content: {
+    backgroundColor: colors.white,
+    borderRadius: 30,
+    flex: 1,
+    marginTop: -30,
+    paddingtop: 15,
+  }
+})
